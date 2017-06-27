1a3df49a322e180dc3886b7332e8b4220e2a79f3
Trovarius <lpereira@I4proinfo.local>
2017-06-27 09:51:23 -0300

    add engine templates JADE/Handlrbars

--- server.js
+++ server.js
@@ -7,8 +7,16 @@ const PORT = 8080;
 
 // App
 const app = express();
+
+app.use(express.static('public'));
+app.set('views', './src/views');
+app.set('views engine', 'jade');
+
+//to use handlebars as engine template discomment next line
+//app.set('view engine', 'hbs'); 
+
 app.get('/', function (req, res) {
-  res.send('Hello world\n');
+  res.render('index', {server_list: ['a', 'b']});
 });
 
 app.listen(PORT);
