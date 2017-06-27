3d4fd5446fbb12012552910debef777778e85bc7
Trovarius <lpereira@I4proinfo.local>
2017-06-27 16:53:42 -0300

    adding route

--- server.js
+++ server.js
@@ -7,11 +7,23 @@ const PORT = 8080;
 
 // App
 const app = express();
+const bookRouter = express.Router();
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 app.set('views engine', 'jade');
 
+bookRouter.route('/')
+		.get(function(req, res){
+			res.send('/Books')
+		});
+
+bookRouter.route('/single')
+		.get(function(req, res){
+			res.send('/Books/Single')
+		});
+
+app.use('/Books', bookRouter);
 //to use handlebars as engine template discomment next line
 //app.set('view engine', 'hbs'); 
 
